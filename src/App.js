c35e9539cfc3ce7d5c65b659dadcb8edfe9e1525
SubhamGupta007 <subhamyoyo23@gmail.com>
2022-01-09 16:44:29 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Header from './Components/Home/Header';
 import Footer from './Components/Home/Footer';
 import Banner from './Components/Home/Banner';
+import Form from './Components/Register/Form';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
     <Header/>
-    <Banner/>>
+    <Banner/>
+    <Form/>
     <Footer/>
+
      
     </div>
   );
