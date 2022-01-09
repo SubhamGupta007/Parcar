e4423e7558bf328649990c8037b7d9d5c652d511
DebuG6290 <goyaldevansh19991999@gmail.com>
2022-01-09 16:54:16 +0530

    added google login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
+import React, { useState } from 'react';
 import Header from './Components/Home/Header';
 import Footer from './Components/Home/Footer';
 import Banner from './Components/Home/Banner';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import UserHome from './Components/UserHome/UserHome'
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-    <Banner/>>
-    <Footer/>
-     
-    </div>
-  );
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [userGoogleData, setUserGoogleData] = useState()
+  let handleLoggedIn=(response)=>{
+    setUserGoogleData(response);
+    setIsUserLoggedIn(true);
+  }
+  if(isUserLoggedIn){
+    return <UserHome/>
+  }
+  else{
+   return (
+      <div className="App">
+      <Header handleLoggedIn={handleLoggedIn}/>
+      <Banner/>
+      <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
